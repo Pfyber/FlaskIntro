f9f33fa541919b445291c78c2501056ec75bf0a1
Pfyber <luka.colaric@gmail.com>
2026-02-17 12:15:54 +0000

    add esp32

--- main.py
+++ main.py
@@ -5,15 +5,22 @@ app = Flask(__name__)
 
 chuck_url = "https://api.chucknorris.io/jokes/random"
 
+temperature = []
 @app.route("/")
 def index():
     call = requests.get(chuck_url).json()
     joke = call["value"]
     name = "Luka Colarič"
-    return render_template("index.html", author = joke)
+    return render_template("index.html", author = temperature)
 
 @app.route("/about")
 def about():
     return "<h1>Welcome to about page</h1>"
 
+@app.route("/data/<temp>")
+def data(temp):
+    print(temp)
+    temperature.append(temp)
+    print(temperature)
+    return f"Got temp {temp}"
 app.run(debug = True)
